657ddc3de29f6d697d44280260e2fdd7e630edc0
Merul Panwala <mmp@narola.email>
2024-01-02 16:33:46 +0530

    Remove unwated console.log

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -10,7 +10,7 @@ const Login = () => {
     const [password, setPassword] = useState("")
     const [isApiCalling, setIsApiCalling] = useState(false)
     const router = useRouter()
-    console.log('isApiCalling', isApiCalling)
+
     const submitHandler = async (e) => {
         e.preventDefault()
 
